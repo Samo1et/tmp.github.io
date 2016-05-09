e444f71c1d304b0b79d7e5e4defb3957dc36824d
Samo1et <studio-sam@mail.ru>
2016-05-09 20:26:22 +0700

    Добавил jQuery переписал код JS

--- js/common.js
+++ js/common.js
@@ -1,45 +1,29 @@
-// Функция scrinBlock показывает блок с формой заказа
-// Функция addBlok добавляет новый блок с формой заказа продукции (не больше 10 блоков)
+	$("#black").bind("change click", function () {
+		if (this.checked){
+			$('.page-order__form-order-black').show();
+		} 
+		else {
+			$('.page-order__form-order-black').hide();
+		}});
 
-function scrinBlock() {
-	checkBoxBlack.addEventListener('click', function() {
-		if (formOrderBlack.classList.contains('page-order__form-order--closed')) {
-			formOrderBlack.classList.remove('page-order__form-order--closed');
-			formOrderBlack.classList.add('page-order__form-order--opened');
-		} else {
-			formOrderBlack.classList.add('page-order__form-order--closed');
-			formOrderBlack.classList.remove('page-order__form-order--opened'); 
-		}
-	});
+	$("#white").bind("change click", function () {
+		if (this.checked){
+			$('.page-order__form-order-white').show();
+		} 
+		else {
+			$('.page-order__form-order-white').hide();
+		}});
 
-	checkBoxWhite.addEventListener('click', function() {
-		if (formOrderWhite.classList.contains('page-order__form-order--closed')) {
-			formOrderWhite.classList.remove('page-order__form-order--closed');
-			formOrderWhite.classList.add('page-order__form-order--opened');
-		} else {
-			formOrderWhite.classList.add('page-order__form-order--closed');
-			formOrderWhite.classList.remove('page-order__form-order--opened'); 
-		}
-	});
+	$("#colors").bind("change click", function () {
+		if (this.checked){
+			$('.page-order__form-order-colors').show();
+		} 
+		else {
+			$('.page-order__form-order-colors').hide();
+		}});
 
-	checkBoxColors.addEventListener('click', function() {
-		if (formOrderColors.classList.contains('page-order__form-order--closed')) {
-			formOrderColors.classList.remove('page-order__form-order--closed');
-			formOrderColors.classList.add('page-order__form-order--opened');
-		} else {
-			formOrderColors.classList.add('page-order__form-order--closed');
-			formOrderColors.classList.remove('page-order__form-order--opened'); 
-		}
-	});
-}
 
-function set(){
 
-	var insertCode = ['<select name="color" id="color"><option value="Выберите марку">Выберите марку</option>','<a class="page-order__form-link" href="masterbatch.html">Подробнее о марках</a>'];
-	document.getElementById('addBlackform').innerHTML = insertCode[1];
-
-	
-};
 
 
 
